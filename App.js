0fa0c17f630e8af63e7d167c1208ffb9330d33e2
Tanya <keratilemampheko20@gmail.com>
2022-09-16 10:36:34 +0200

    first commit

--- App.js
+++ App.js
@@ -1,25 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image,  StyleSheet, Text, View } from 'react-native';
+import { Image, Dimensions,  StyleSheet, Text, View } from 'react-native';
+const { width } = Dimensions.get('screen');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Happy Birthday</Text>
-      <Text>I hope you have a wonderful birthday!</Text>
-      <StatusBar style="auto" />
+        <h3 class="back">HAPPY BIRTHDAY!</h3>
+      <Image source={require('./assets/cake2.png')} style={styles.cake}/>
+     <p>Having the opportunity to wish a happy birthday and celebrate the life of a great friend like you is a true gift!! Have a great day today and the year ahead is full of many blessings.</p>
+     <p class="name">Tanya</p>
     </View>
-
+  
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'pink',
+    backgroundColor: '#FFC0CB',
     alignItems: 'center',
     justifyContent: 'center',
-    txt: {
-
+    },
+    cake: {
+      width,
+      height: width * 1.2,
+      resizeMode: 'cover'
     }
-  },
 });
